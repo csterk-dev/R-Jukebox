1f749209e1c1a04d46e425321b0d97c593a98754
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2024-06-01 23:36:50 +1000

    add socket.ts jsdoc comment

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,12 @@
 import { SOCKET_URL } from "../constants";
 import { io } from "socket.io-client";
 
-
-/** The socket instance hosted on the server. */
+/** 
+ * The socket instance hosted on the server. 
+ * 
+ * This is delcared here to prevent 'Websocket Connection Error Insufficient Resources' error 
+ * that would occur if we use the socket directly in the useWebSocket hook.
+ * 
+ * See [this stack overflow article for more info](https://stackoverflow.com/questions/69008820/websocket-connection-error-insufficient-resources).
+ */
 export const socket = io(SOCKET_URL);
