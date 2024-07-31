dfad38cb652d9bc1d15df30256a22072353bd147
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2024-07-31 19:22:16 +1000

    implement sorted history

--- src/components/molecules/QueueHistoryTabs.tsx
+++ src/components/molecules/QueueHistoryTabs.tsx
@@ -1,7 +1,9 @@
-import { Flex, FlexProps, Tab, TabList, TabPanel, TabPanels, Tabs, useColorModeValue } from "@chakra-ui/react";
-import { FC, memo } from "react"
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Flex, FlexProps, Tab, TabList, TabPanel, TabPanels, Tabs, Text, useColorModeValue } from "@chakra-ui/react";
+import { VideoCard } from "components/atoms/VideoCard";
+import { FC, memo, useCallback, useMemo } from "react"
 import { useAppState } from "state/appContext";
-
+import { usePlayer } from "state/playerContext";
+import { videoPlayedAtToString } from "utils/misc";
 
 type QueueHistoryProps = FlexProps;
 
@@ -9,6 +11,66 @@ const _QueueHistoryTabs: FC<QueueHistoryProps> = ({ ...props }) => {
   const foreground = useColorModeValue("rgba(255, 255, 255, 0.8)", "rgba(13, 15, 24, 0.75)");
   const tabHeader = useColorModeValue("neutral.white", "neutral.900");
   const { isMobile } = useAppState();
+  const { history, playVideo } = usePlayer();
+
+
+  /** Callback to play the selected video from the history. */
+  const onClickHistoryVideo = useCallback((video: Video) => {
+    playVideo(video);
+  }, [playVideo]);
+
+
+  type SortedHistory = {
+    [key: string]: HistoryVideo[];
+  }
+
+  const historyCards: JSX.Element[] = useMemo(() => {
+    const sortedHistory = history.reduce((grouped: SortedHistory, video: HistoryVideo) => {
+      const vidDate = video.playedDate;
+      if (!grouped[vidDate]) grouped[vidDate] = [];
+
+      grouped[vidDate].push(video);
+      return grouped;
+    }, {} as SortedHistory);
+
+    const jsx: JSX.Element[] = [];
+    Object.entries(sortedHistory).forEach(([date, videos]) => {
+      jsx.push(
+        <AccordionItem borderColor="transparent">
+          {({ isExpanded }) => (
+            <>
+              <h3>
+                <AccordionButton
+                  display="flex"
+                  flex="1"
+                  fontSize="12"
+                  justifyContent="space-between"
+                  p="10px"
+                >
+                  <Text>{videoPlayedAtToString(date)}</Text>
+                  <Text>{!isExpanded ? "Show more" : "Hide"}</Text>
+                </AccordionButton>
+              </h3>
+              <AccordionPanel px="10px" py={0}>
+                <Flex flexDirection="column" gap="10px">
+                  {videos.map(vid => (
+                    <VideoCard
+                      key={`${vid.videoId}${vid.playedAt}`}
+                      isMobile={isMobile}
+                      playVideo={onClickHistoryVideo}
+                      video={vid}
+                    />
+                  ))}
+                </Flex>
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+      )
+    })
+
+    return jsx;
+  }, [isMobile, onClickHistoryVideo, history]);
 
 
   return (
@@ -25,6 +87,7 @@ const _QueueHistoryTabs: FC<QueueHistoryProps> = ({ ...props }) => {
     >
       <Tabs
         colorScheme="purple"
+        defaultIndex={1}
         variant="soft-rounded"
         width="100%"
         isLazy
@@ -33,12 +96,21 @@ const _QueueHistoryTabs: FC<QueueHistoryProps> = ({ ...props }) => {
           <Tab color="current">Queue</Tab>
           <Tab color="current">History</Tab>
         </TabList>
-        <TabPanels px="10px">
-          <TabPanel p={0} py="10px">
+        <TabPanels>
+          <TabPanel p={0} px="10px" py="10px">
             <p>Coming soon</p>
           </TabPanel>
-          <TabPanel p={0} py="10px">
-            <p>Coming soon</p>
+          <TabPanel p={0} py="0px">
+            {history.length === 0 ?
+              <p>Nothing to display</p> :
+              <Accordion
+                defaultIndex={[0]}
+                variant="unstyled"
+                allowMultiple
+              >
+                {historyCards}
+              </Accordion>
+            }
           </TabPanel>
         </TabPanels>
       </Tabs>
