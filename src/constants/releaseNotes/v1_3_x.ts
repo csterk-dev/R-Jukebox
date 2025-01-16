efa4199fb2473feb62990aac8ee1a186bb2d7499
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2025-01-16 22:13:30 +1000

    v1.3.1

--- src/constants/releaseNotes/v1_3_x.ts
+++ src/constants/releaseNotes/v1_3_x.ts
@@ -10,8 +10,33 @@ const v1_3_0: ReleaseNote[] = [
   }
 ];
 
+const v1_3_1: ReleaseNote[] = [
+  {
+    details: "Queue now displays the total length of all queued items.",
+    kind: "newFeature"
+  },
+  {
+    details: "Updated queue messages to be more descriptive.",
+    kind: "improvement"
+  },
+  {
+    details: "Updated background animations.",
+    kind: "improvement"
+  },
+  {
+    details: "Updated to Chakra UI 2.10.",
+    kind: "improvement"
+  }
+];
+
 
 export const v1_3ReleaseNotes: ReleaseNotes = [
+  {
+    title: "Welcome to 2025!",
+    versionNum: "1.3.1",
+    notes: v1_3_1,
+    date: "17/01/2024"
+  },
   {
     title: "Introducing auto complete!",
     versionNum: "1.3.0",
