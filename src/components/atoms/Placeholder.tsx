3c0161632c3500daeae88566747813cec43de781
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2025-06-23 22:09:05 +1000

    fix placeholder text color

--- src/components/atoms/Placeholder.tsx
+++ src/components/atoms/Placeholder.tsx
@@ -31,7 +31,7 @@ export const Placeholder: FC<PlaceholderProps> = ({ title, icon, iconBg, iconBgD
           fontSize="70px"
         />
       </Box>
-      <Text color="text.body.subtle" textAlign="center">{title}</Text>
+      <Text color="text.body" textAlign="center">{title}</Text>
     </Flex>
   )
 }
