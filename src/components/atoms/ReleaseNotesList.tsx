ae9625decec2772a5194e75768f39443900f9eb4
Chris Sterk <chris.sterkenburg@usesoftware.com.au>
2026-01-10 15:29:38 +1000

    Fix list item styling

--- src/components/atoms/ReleaseNotesList.tsx
+++ src/components/atoms/ReleaseNotesList.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Icon, List } from "@chakra-ui/react";
+import { List } from "@chakra-ui/react";
 import { FC } from "react";
 import { HiBugAnt, HiStar, HiWrenchScrewdriver } from "react-icons/hi2";
 
@@ -10,19 +10,13 @@ type NotesProps = { notes: ReleaseNoteEntry[]; }
  */
 export const ReleaseNotesList: FC<NotesProps> = ({ notes, ...props }) => {
   return (
-    <List.Root gap={2} {...props}>
+    <List.Root gap={2} variant="plain" {...props}>
       {notes.map(note => (
         <List.Item key={note.details}>
-          <Flex gap={2}>
-            <Box mt="2px">
-              <Icon
-                aria-roledescription="Bullet point"
-                as={note.kind == "bugFix" ? HiBugAnt : note.kind == "improvement" ? HiWrenchScrewdriver : HiStar}
-                color={note.kind == "bugFix" ? "red.300" : note.kind == "improvement" ? "primary.300" : "yellow.500"}
-              />
-            </Box>
-            <Flex flex={1}>{note.details}</Flex>
-          </Flex>
+          <List.Indicator color={note.kind == "bugFix" ? "red.300" : note.kind == "improvement" ? "primary.300" : "yellow.500"} asChild>
+            {note.kind == "bugFix" ? <HiBugAnt /> : note.kind == "improvement" ? <HiWrenchScrewdriver /> : <HiStar />}
+          </List.Indicator>
+          {note.details}
         </List.Item>
       ))}
     </List.Root>
